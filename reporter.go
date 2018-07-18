c298e44fe041942d7c0130a99dc730a0c0c4ab3e
capitancambio <capitan.cambio@gmail.com>
2018-07-18 10:40:45 +0100

    Fix panic treatment

--- reporter.go
+++ reporter.go
@@ -2,6 +2,7 @@ package reporter
 
 import (
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/onsi/ginkgo/config"
@@ -30,27 +31,29 @@ func (reporter *GoTestCompatibleReporter) BeforeSuiteDidRun(setupSummary *types.
 
 func (reporter *GoTestCompatibleReporter) SpecWillRun(specSummary *types.SpecSummary) {
 	fmt.Println("=== RUN  ", createTestName(specSummary.ComponentTexts))
-
+}
 
 func (reporter *GoTestCompatibleReporter) SpecDidComplete(specSummary *types.SpecSummary) {
 	var header string = "--- "
-
 	postfix := "\n" + specSummary.CapturedOutput
 	if specSummary.Passed() {
 		header += "PASS"
 	} else if specSummary.Failed() {
 		header += "FAIL"
 	} else if specSummary.Panicked() {
-		header += "PANIC"
+		header = "panic: " + specSummary.Failure.ForwardedPanic + " [recovered]    "
+		postfix += fmt.Sprintf("Location: %v \nMessage: %v\n", specSummary.Failure.ComponentCodeLocation.String(), specSummary.Failure.Message)
 	} else if specSummary.Skipped() {
 		header += "SKIP"
 	} else if specSummary.Pending() {
 		header += "PENDING"
 	}
-	fmt.Printf("%s: %s (%0.2f)s\n",
-		header,
-		createTestName(specSummary.ComponentTexts),
-		(float64(specSummary.RunTime) / float64(time.Second)))
+	if header != "--- " {
+		fmt.Printf("%s: %s (%0.2f)s\n",
+			header,
+			createTestName(specSummary.ComponentTexts),
+			(float64(specSummary.RunTime) / float64(time.Second)))
+	}
 	if postfix != "" {
 		fmt.Println(postfix)
 	}
